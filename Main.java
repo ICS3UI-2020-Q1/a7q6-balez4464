56e7376e0f6230e345a7e8e6b494e0a8703fc018
balez4464 <balez4464@wrdsb.ca>
2020-10-14 13:41:34 +0000

    made a program that says true if all the digits are odd but says false if there are any even

--- Main.java
+++ Main.java
@@ -1,15 +1,36 @@
 /**
- *
- * @author 
+ *This program returns true if all the digits are odd but returns false if there are any even numbers
+ * @author Zachary Balean
  */
 public class Main {
+
+  //Methhos for all the odd digits
+  public static boolean allDigitsOdd(int number){
+    //integer for figuring out if it is odd or even
+    int remainder = 0;
+    //while loop to go through the digitsg
+    while(number >= 1){
+      //calcualtes the digit number
+      remainder = number % 10;
+      //goes on to the next digit of the nuber
+      number = number / 10;
+      //if statement to return false if even
+      if(remainder % 2 == 0){
+        return false;
+      } 
+  }
+    //return true if odd
+    return true;
+}
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // calls the method
+    boolean answer = allDigitsOdd(135319);
+    //prints out answer
+    System.out.println(answer);
   }
 }
